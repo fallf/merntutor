6362490e93df7347202d61aef10265a54b975314
fatou fall <fatoufall95@gmail.com>
2025-01-17 11:33:49 -0500

    rest api for goals full CRUD

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,10 +1,12 @@
 const asyncHandler = require("express-async-handler");
-
+const Goal = require("../models/goalModel");
+const { text } = require("express");
 // @desc    GET goals
 // @route GET api/goals
 //@access Private
 const getGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "get goals" });
+  const goals = await Goal.find();
+  res.status(200).json(goals);
 });
 
 // @desc    SET goal
@@ -15,21 +17,46 @@ const setGoal = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("please add a text field");
   }
-  res.status(200).json({ message: "set goals" });
+  const goal = await Goal.create({
+    text: req.body.text,
+  });
+
+  res.status(200).json(goal);
 });
 
 // @desc    UPDATE goals
 // @route PUT api/goals/:id
 //@access Private
 const updateGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update goal ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found ");
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.status(200).json(updatedGoal);
 });
 
 // @desc    DELETE goals
 // @route DELETE api/goals/:id
 //@access Private
 const deleteGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete goal ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    res.status(404); // Goal not found, 404 is the appropriate status code.
+    throw new Error("Goal not found");
+  }
+
+  // Use deleteOne() or delete() to remove the document.
+  await Goal.deleteOne({ _id: req.params.id });
+
+  res
+    .status(200)
+    .json({ message: `Goal with ID ${req.params.id} has been deleted` });
 });
 
 module.exports = {
